3fe8e87f297e0edf8af2d52dc95cc2e1b1be6b50
hafiz <hafiz.abdullahoffical@gmail.com>
2023-11-04 08:53:07 +0500

    add images of business progfile and gig

---
+++ src/components/Business.jsx
@@ -0,0 +1,48 @@
+const BusinessMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.upwork.com/freelancers/~0199a33c600994d315?viewMode=1&s=1110580755107926016"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="upwork-link" src="assets/upwork.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.fiverr.com/hafizabdulla377?up_rollout=true"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="Fiverr-link" src="assets/Fiverr.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.freelancer.com/u/hafizab63861462"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="freelancer-link" src="assets/freelancer.webp" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.guru.com/freelancers/hafiz-abdullah-naeem"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="guru-link" src="assets/guru.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.fiverr.com/s/EvNLy9"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="mePoster-link" src="assets/mePoster.jpeg" />
+      </a>
+    </div>
+  );
+};
+
+export default BusinessMediaIcons;
--- src/scenes/MyGigsLink.jsx
+++ src/scenes/MyGigsLink.jsx
@@ -1,8 +1,8 @@
 // import LineGradient from "../components/LineGradient";
 import { motion } from "framer-motion";
+import BusinessMediaIcons from "../components/Business";
 
 const MyGigsLink = () => {
-
   const container = {
     hidden: {},
     visible: {
@@ -12,7 +12,6 @@ const MyGigsLink = () => {
     },
   };
 
-  
   return (
     <section id="gigs" className="pt-10 pb-24">
       <motion.div
@@ -28,12 +27,12 @@ const MyGigsLink = () => {
       >
         <div>
           <p className="font-playfair font-semibold text-4xl">
-            <span className="text-red">My</span> Gigs
+            <span className="text-red">Bussine</span> Account
           </p>
         </div>
       </motion.div>
 
-      <div className="flex justify-center">
+      {/* <div className="flex justify-center">
         <motion.div
           className="sm:grid sm:grid-cols-3"
           variants={container}
@@ -44,7 +43,21 @@ const MyGigsLink = () => {
       
 
         </motion.div>
-      </div>
+      </div> */}
+
+      <motion.div
+        className="flex mt-5 justify-center md:justify-start"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ delay: 0.4, duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, x: -50 },
+          visible: { opacity: 1, x: 0 },
+        }}
+      >
+        <BusinessMediaIcons />
+      </motion.div>
     </section>
   );
 };
